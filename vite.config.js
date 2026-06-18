c1950e11317a5f7354100b4ba5b0fa40bb4984ff
Louisa <louisa.peeters@howest.student.be>
2026-06-18 10:47:33 +0200

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+base: '/made_manual/',
   plugins: [react()],
   server: {
     port: 3000,
